0e91abf138765a02cd313f8f7dff98c65a75cb55
Finn Harms <github@finn-harms.de>
2024-02-02 02:22:05 +0100

    login

--- frontend/src/main.py
+++ frontend/src/main.py
@@ -1,6 +1,11 @@
 import requests
 import datetime
-from nicegui import ui
+from typing import Optional
+
+from fastapi import Request
+from fastapi.responses import RedirectResponse
+from starlette.middleware.base import BaseHTTPMiddleware
+from nicegui import Client, app, ui
 import requests
 import locale
 from overview import overview_page
@@ -10,6 +15,26 @@ locale.setlocale(locale.LC_ALL, 'de_DE')
 
 session = requests.Session()
 
+passwords = {'mark': 'marki', 'finn': 'finni', "daniela": "dani"}
+
+unrestricted_page_routes = {'/login'}
+
+
+class AuthMiddleware(BaseHTTPMiddleware):
+    """This middleware restricts access to all NiceGUI pages.
+
+    It redirects the user to the login page if they are not authenticated.
+    """
+    async def dispatch(self, request: Request, call_next):
+        if not app.storage.user.get('authenticated', False):
+            if request.url.path in Client.page_routes.values() and request.url.path not in unrestricted_page_routes:
+                app.storage.user['referrer_path'] = request.url.path  # remember where the user wanted to go
+                return RedirectResponse('/login')
+        return await call_next(request)
+
+
+app.add_middleware(AuthMiddleware)
+
 @ui.page('/')
 async def index_page():
     await overview_page(session)
@@ -18,4 +43,21 @@ async def index_page():
 async def event_page(date: str):
     await detail_page(session, date)
 
-ui.run()
+@ui.page('/login')
+def login() -> Optional[RedirectResponse]:
+    def try_login() -> None:  # local function to avoid passing username and password as arguments
+        if passwords.get(username.value) == password.value:
+            app.storage.user.update({'username': username.value, 'authenticated': True})
+            ui.open(app.storage.user.get('referrer_path', '/'))  # go back to where the user wanted to go
+        else:
+            ui.notify('Wrong username or password', color='negative')
+
+    if app.storage.user.get('authenticated', False):
+        return RedirectResponse('/')
+    with ui.card().classes('absolute-center'):
+        username = ui.input('Username').on('keydown.enter', try_login)
+        password = ui.input('Password', password=True, password_toggle_button=True).on('keydown.enter', try_login)
+        ui.button('Log in', on_click=try_login)
+    return None
+
+ui.run(storage_secret='psssst')
